0b400e67e3cfc34b8f54fc4c933645b3dc4f9a77
wesley <rladhlthf123@naver.com>
2022-05-14 17:18:55 +0900

    make UI by using mustache

---
+++ src/test/java/com/wesley/stock/web/IndexControllerTest.java
@@ -0,0 +1,29 @@
+package com.wesley.stock.web;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+public class IndexControllerTest {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void loadMainPage() {
+        // when
+        String body = this.restTemplate.getForObject("/", String.class);
+
+        // then
+        assertThat(body).contains("주식 종목 제공 서비스");
+    }
+
+}
