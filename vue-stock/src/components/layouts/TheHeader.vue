3108104aeec29d1a23ac2c6c782c0e299e1fdc2a
wesley <rladhlthf123@naver.com>
2023-02-04 15:53:50 +0900

    Header 추가

---
+++ vue-stock/src/components/layouts/TheHeader.vue
@@ -0,0 +1,22 @@
+<template>
+  <!-- <nav class="navbar navbar-expand-sm navbar-dark bg-primary"> -->
+    <div>
+      <h1>&nbsp;주식 종목 제공 서비스</h1>
+      <!-- <RouterLink class="navbar-brand" to="/">STOCK INFO.</RouterLink> -->
+      <!-- <button class="navbar-toggler" type="button" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button> -->
+    </div>
+  <!-- </nav> -->
+
+<!--  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">-->
+</template>
+
+<script>
+export default {
+  name: "TheHeader",
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
