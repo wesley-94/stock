57aa2d953109973988bd680c784ab57f85425edf
wesley <rladhlthf123@naver.com>
2023-02-05 23:00:34 +0900

    Footer 추가

---
+++ vue-stock/src/components/layouts/TheFooter.vue
@@ -0,0 +1,23 @@
+<template>
+    <hr />
+    <div class="container">
+        <p>&nbsp;&nbsp;Copyright © 2023</p>
+        <ul>
+            <li>Contact: {{ email }}</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "TheFooter",
+    data() {
+        return {
+            email: "rladhlthf123@naver.com",
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
