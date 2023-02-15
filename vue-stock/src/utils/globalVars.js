b21c5fb8ee9568d398d376384fe747ab02c55f73
wesley <rladhlthf123@naver.com>
2023-02-15 23:46:56 +0900

    전역 변수 관리 파일 추가

--- vue-stock/src/utils/globalVars.js
+++ vue-stock/src/utils/globalVars.js
@@ -5,8 +5,14 @@ axios.defaults.headers['Content-Type'] = 'application/json;charset=utf-8';
 axios.defaults.headers['Access-Control-Allow-Origin'] = '*';
 axios.defaults.headers['Access-Control-Allow-Credentials'] = true;
 
+const axiosInstance = axios.create();
+
 export default {
     install(Vue) {
+        // 1. axios 변수 설정
         Vue.config.globalProperties.$axios = axios;
+
+        // 2. axiosInstance 변수 설정
+        Vue.config.globalProperties.$axiosInstance = axiosInstance;
     }
 }
