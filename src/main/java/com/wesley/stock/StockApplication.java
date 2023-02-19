f3c2b2fda0fdf0932f4ec1ebf1edf55734e7850e
wesley <rladhlthf123@naver.com>
2023-02-19 22:23:32 +0900

    Initializer 수정

--- src/main/java/com/wesley/stock/StockApplication.java
+++ src/main/java/com/wesley/stock/StockApplication.java
@@ -2,9 +2,10 @@ package com.wesley.stock;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class) // Spring Security 기능 비활성화
 public class StockApplication {
 
 	public static void main(String[] args) {
