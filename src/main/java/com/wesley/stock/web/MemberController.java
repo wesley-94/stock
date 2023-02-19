a76c3aea6ab7a568f8a81030929c48e9ef4f8a4e
wesley <rladhlthf123@naver.com>
2023-02-19 22:22:49 +0900

    회원 가입, 목록 Controller 수정

--- src/main/java/com/wesley/stock/web/MemberController.java
+++ src/main/java/com/wesley/stock/web/MemberController.java
@@ -7,13 +7,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-
-import javax.validation.Valid;
+import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 public class MemberController {
 
@@ -26,29 +23,31 @@ public class MemberController {
     }
 
     @PostMapping(value = "/members/new")
-    public String create(@Valid MemberForm form, BindingResult result) {
+    public void create(@RequestBody MemberForm form) {
+//    public void create(@Valid MemberForm form, BindingResult result) {
 
-        if (result.hasErrors()) {
-            return "members/createMemberForm";
-        }
+//        if (result.hasErrors()) {
+//            return "members/createMemberForm";
+//        }
 
         Address address = new Address(form.getCity(), form.getStreet(), form.getZipcode());
         Member member = new Member();
         member.setName(form.getName());
+
         member.setAddress(address);
 
         memberService.join(member);
 
-        return "redirect:/";
-
+//        return "redirect:/";
     }
 
     @GetMapping(value = "/members")
-    public String list(Model model) {
+    public List list(Model model) {
         List<Member> members = memberService.findMembers();
         model.addAttribute("members", members);
 
-        return "members/memberList";
+//        return "members/memberList";
+        return members;
     }
 
 }
