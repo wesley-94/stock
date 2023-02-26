702fe15388aaa393d58077f429bd1c1d01011809
wesley <rladhlthf123@naver.com>
2023-02-26 21:31:51 +0900

    메인 화면 수정

--- vue-stock/src/views/HomeView.vue
+++ vue-stock/src/views/HomeView.vue
@@ -10,19 +10,19 @@
       <br>
       <p class="lead">회원 기능</p>
       <p class="lead">
-        <a class="btn btn-lg btn-secondary" href="/members/new">회원 가입</a>
+        <a class="btn btn-lg btn-secondary me-2" href="/members/new">회원 가입</a>
         <a class="btn btn-lg btn-secondary" href="/members">회원 목록</a>
       </p>
       <br my-4>
       <p class="lead">종목 기능</p>
       <p class="lead">
-        <a class="btn btn-lg btn-dark" href="/stocks/new">주식 등록</a>
+        <a class="btn btn-lg btn-dark me-2" href="/stocks/new">주식 등록</a>
         <a class="btn btn-lg btn-dark" href="/stocks">주식 목록</a>
       </p>
       <br my-4>
       <p class="lead">거래 기능</p>
       <p class="lead">
-        <a class="btn btn-lg btn-info" href="/trade">주식 거래</a>
+        <a class="btn btn-lg btn-info me-2" href="/trade">주식 거래</a>
         <a class="btn btn-lg btn-info" href="/trades">거래 내역</a>
       </p>
     </div>
