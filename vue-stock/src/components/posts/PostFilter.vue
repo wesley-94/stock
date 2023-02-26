33e87bc532eafa62f7bc751df9dfe8498c6c03df
wesley <rladhlthf123@naver.com>
2023-02-26 21:28:36 +0900

    검색 필터 추가

---
+++ vue-stock/src/components/posts/PostFilter.vue
@@ -0,0 +1,32 @@
+<template>
+    <!-- 표시 건수 제한 -->
+    <div class="row g-3">
+        <div class="col"></div>
+        <div class="col-1">
+            <select 
+                :value="limit"
+                @input="$emit('update:limit', $event.target.value)"
+                class="form-select"
+            >
+                <option value=1>1건</option>
+                <option value=3>3건</option>
+                <option value=5>5건</option>
+                <option value=10>10건</option>
+            </select>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { defineProps, defineEmits } from 'vue';
+
+defineProps({
+    limit: [Number, String],
+})
+
+defineEmits(['update:limit']);
+
+</script>
+
+<style lang="scss" scoped>
+</style>
