175c72bda73d1ec19e3693bba51d3e242cebc00e
wesley <rladhlthf123@naver.com>
2023-02-26 21:31:33 +0900

    회원 정보 조회 로직 수정

--- vue-stock/src/views/members/MemberList.vue
+++ vue-stock/src/views/members/MemberList.vue
@@ -1,52 +1,93 @@
 <template>
-    <div class="col-md-12">
-        <!-- 목록 출력 영역 -->
-        <table class="table table-striped">
-            <thead class="thead-strong">
-            <tr>
-                <th>ID</th> 
-                <th>이름</th> 
-                <th>도시</th> 
-                <th>거리</th> 
-                <th>우편번호</th> 
-            </tr>
-            </thead>
-            <tbody id="tbody">
-                <!-- members 정보 가져와야 함 / 현재는 하드코딩 -->
-                <tr v-for='(item, index) in dataTable' :key='index'>
-                    <td>{{ item.id }}</td>
-                    <td>{{ item.name }}</td>
-                    <td>{{ item.address.city }}</td>
-                    <td>{{ item.address.street }}</td>
-                    <td>{{ item.address.zipcode }}</td>
-                </tr>
-            </tbody>
-        </table>
-        <div class="text_center">
+    <h3 class="text_center mt-3">회원 정보</h3>
+    <hr>
+    <!-- 표시 건수 제한 -->
+    <PostFilter
+        :limit="searchParam.pageLimit"
+        @update:limit="changeLimit"
+    />
+    <template v-if=!isExist>
+        <p class="text-center py-4 text-muted">No Results</p>
+    </template>
+    <template v-else>
+        <div class="mt-3 col-md-12">
+            <!-- 목록 출력 영역 -->
+            <table class="table table-striped">
+                <thead class="thead-strong">
+                    <th v-for='(item, index) in title' :key='index'>{{ item }}</th>
+                </thead>
+                <tbody id="tbody">
+                    <tr v-for='(item, index) in dataTable' :key='index'>
+                        <td>{{ item.id }}</td>
+                        <td>{{ item.name }}</td>
+                        <td>{{ item.address.city }}</td>
+                        <td>{{ item.address.street }}</td>
+                        <td>{{ item.address.zipcode }}</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+
+        <!-- Pagination -->
+        <AppPagination 
+            :current-page="searchParam.currentPage"
+            :page-count="pageCount"
+            @page="page => (searchParam.currentPage = page)"
+        />
+        
+        <div class="mt-2 text_center">
             <button class="btn btn-secondary" @click="goHomePage">메인 페이지로 이동</button>
         </div>
-    </div>
+    </template>
 </template>
 
 <script setup>
-import { ref } from 'vue';
+import { ref, computed, watchEffect } from 'vue';
 import { useRouter } from 'vue-router';
 import { http } from '@/api/index.js';
+import PostFilter from '@/components/posts/PostFilter.vue';
+import AppPagination from '@/components/apps/AppPagination.vue';
 
 const router = useRouter();
 const dataTable = ref([]);
+const totalCount = ref(null);
+const isExist = ref(null);
+
+const title = ["ID", "이름", "도시", "거리", "우편번호"];
+
+const searchParam = ref({
+    // pagination vars
+    currentPage: 1, // 초기값
+    pageLimit: 5, // 초기값
+});
+
+const changeLimit = value => {
+    searchParam.value.pageLimit = parseInt(value);
+    searchParam.value.currentPage = 1;
+}
 
 const searchList = async () => {
-    const { data } = await http.get('/members', {});
-    dataTable.value = data;
+    const { data } = await http.post('/members', { ...searchParam.value });
+    console.log(data);
+    totalCount.value = data.count;
+    dataTable.value = data.members;
+    isExist.value = (data.members.length > 0);
 };
 
-searchList();
+watchEffect(searchList);
+
+const pageCount = computed(() => 
+    Math.ceil(totalCount.value / searchParam.value.pageLimit),
+);
 
 const goHomePage = () => {
     router.push('/');
 }
+
 </script>
 
-<style lang="scss" scoped>
+<style>
+.text_right {
+    text-align: right;
+}
 </style>
