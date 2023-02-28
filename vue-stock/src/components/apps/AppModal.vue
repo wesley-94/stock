3286aa15cbcabbca1701678f3920430c82622337
wesley <rladhlthf123@naver.com>
2023-02-28 22:29:33 +0900

    모달 기능 추가

---
+++ vue-stock/src/components/apps/AppModal.vue
@@ -0,0 +1,50 @@
+<template>
+    <!-- Modal -->
+    <div v-if="modelValue">
+        <div class="modal-backdrop fade show"></div>
+        <div class="modal fade show d-block" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <slot name="header">
+                            <h4 class="me-2 modal-title" id="exampleModalLabel">{{ title }}</h4>
+                            <button type="button" class="btn-close" aria-label="Close" @click="$emit('update:modelValue', false)"></button>
+                        </slot>
+                    </div>
+                    <div class="modal-body">
+                        <slot></slot>
+                    </div>
+                    <div class="modal-footer">
+                        <slot name="actions"></slot>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { defineProps, defineEmits } from 'vue';
+
+defineProps({
+    modelValue: Boolean,
+    title: String,
+})
+
+defineEmits(['close', 'update:modelValue']);
+</script>
+
+<style scoped>
+.v-enter-from,
+.v-leave-to {
+	opacity: 0;
+}
+.v-enter-active,
+.v-leave-active {
+	transition: all 0.5s ease;
+}
+.v-enter-to,
+.v-leave-from {
+	opacity: 1;
+}
+</style>
---
+++ vue-stock/src/components/posts/PostModal.vue
@@ -0,0 +1,36 @@
+<template>
+    <AppModal v-model="show" title="알림">
+        <template #default>
+            <div class="row g-3">
+                <div class="text-muted">{{ message }}</div>
+            </div>
+        </template>
+        <template #actions>
+            <button type="button" class="btn btn-primary" @click="closeModal">확인</button>
+            <button type="button" class="btn btn-secondary" @click="closeModal">닫기</button>
+        </template>
+    </AppModal>
+</template>
+
+<script setup>
+import AppModal from '@/components/apps/AppModal.vue';
+import { computed, defineProps, defineEmits } from 'vue';
+const props = defineProps({
+    modelValue: Boolean,
+    message: String,
+});
+const emit = defineEmits(['update:modelValue']);
+
+const show = computed({
+    get() {
+        return props.modelValue
+    },
+    set(value) {
+        emit('update:modelValue', value);
+    },
+})
+
+const closeModal = () => (show.value = false);
+</script>
+
+<style lang="scss" scoped></style>
